2f9aa58b6aaaad75307cd66210dd5d66c98feea5
Radhika Gupta <149066611+Rgupta1745@users.noreply.github.com>
2026-04-03 23:54:05 +0530

    Time: 1 ms (100%), Space: 46.4 MB (74.45%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -29,6 +29,7 @@ class Solution {
     private int dfs(int[][] grid,int i,int j){
         int rows=grid.length;
         int cols=grid[0].length;
+        int area = 1;
 
         if (i<0 || j<0 || i>=rows || j>=cols || grid[i][j]==0) {
             return 0;
