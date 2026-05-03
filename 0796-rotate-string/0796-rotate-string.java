a376ba2a0d8f402d1e30ce66e7f912bb6a797274
Radhika Gupta <149066611+Rgupta1745@users.noreply.github.com>
2026-05-04 00:31:49 +0530

    Time: 0 ms (100%), Space: 42.6 MB (73.32%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,41 +1,9 @@
 class Solution {
-
-    public static void reverse(char[] ch, int i, int k) {
-        while (i < k) {
-            char temp = ch[i];
-            ch[i] = ch[k];
-            ch[k] = temp;
-            i++;
-            k--;
-        }
-    }
-
     public boolean rotateString(String s, String goal) {
-
         if (s.length() != goal.length()) return false;
-
-        // Try all rotations
-        for (int i = 0; i < s.length(); i++) {
-
-            char[] ch = s.toCharArray();
-
-            // Step 1: reverse whole array
-            reverse(ch, 0, ch.length - 1);
-
-            // Step 2: reverse first part
-            reverse(ch, 0, ch.length - i - 1);
-
-            // Step 3: reverse second part
-            reverse(ch, ch.length - i, ch.length - 1);
-
-            // Convert back to string
-            String rotated = new String(ch);
-
-            if (rotated.equals(goal)) {
-                return true;
-            }
+        if((s + s).contains(goal)){
+            return true;
         }
-
         return false;
     }
 }
